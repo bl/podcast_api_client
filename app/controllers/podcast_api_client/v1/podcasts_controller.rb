03fef3a7a7a28cd367ac8f62b5696b2a2e27e1fe
bernard laveaux <blaveaux@outlook.com>
2016-02-02 17:59:43 -0500

    created basic client podcast controller show action

--- app/controllers/podcast_api_client/v1/podcasts_controller.rb
+++ app/controllers/podcast_api_client/v1/podcasts_controller.rb
@@ -1,2 +1,5 @@
 class PodcastApiClient::V1::PodcastsController < ApplicationController
+  def show
+    @podcast = PodcastApiClient::V1::Podcast.find params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'help'    => 'static_pages#help'
   get 'contact' => 'static_pages#contact'
+
+  scope module: :podcast_api_client do
+    scope module: :v1 do
+      resources :podcasts, only: [:show]
+    end
+  end
 end
